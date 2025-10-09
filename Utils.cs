31f7ec245c4996f158fb56457cad2dcda1e66a4d
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-09 09:37:15 +0800

    fix: Update `Utils.cs` and `BepInExUtils.Generator.dll`

--- Utils.cs
+++ Utils.cs
@@ -2,7 +2,7 @@ global using HKS_NoClip.Extensions;
 
 namespace HKS_NoClip;
 
-public static partial class Utils
+public partial class Utils
 {
     internal const string GameName = "Hollow Knight Silksong.exe";
 }
